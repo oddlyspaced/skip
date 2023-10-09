46b34fc02fd4d11fd9e4edc4ed86d30620f5534d
oddlyspaced <oddlyspaced1@gmail.com>
2023-10-09 17:54:46 +0530

    popup: test skipping functionality

--- popup.js
+++ popup.js
@@ -1,20 +1,22 @@
-let changeColor = document.getElementById("testButton");
+let testButton = document.getElementById("testButton");
 
-changeColor.addEventListener("click", async () => {
-  chrome.storage.sync.set({ inputtag: "div" });
-
+testButton.addEventListener('click', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setBorderColor,
-  });
+    function: getVideoPlayer
+  }) 
 });
 
-function setBorderColor() {
-  chrome.storage.sync.get("inputtag", ({ inputtag }) => {
-    document.querySelectorAll(inputtag).forEach((element) => {
-      element.style.border = "1px solid red";
-    });
-  });
+function getVideoPlayer() {
+  console.log("reaching")
+  Array.from(document.getElementsByClassName("html5-video-container")).forEach((element) => {
+    let mainVideoContainer = element.parentNode;
+    let videoPlayer = element.firstChild;
+    console.log(mainVideoContainer.id);
+    console.log(mainVideoContainer.className);
+    console.log(videoPlayer.className);
+    videoPlayer.play();
+    videoPlayer.currentTime = 9999999;
+  })
 }
