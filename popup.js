ec2851ac5ed388507154a960c1cd11f1ec58851d
oddlyspaced <oddlyspaced1@gmail.com>
2023-10-09 21:49:30 +0530

    popup: test seeking + skipping logic in chain

--- popup.js
+++ popup.js
@@ -4,9 +4,50 @@ testButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: seekAd,
+    function: hookVideoPlayer,
   });
 });
+
+function hookVideoPlayer() {
+  const videoContainer = Array.from(
+    document.getElementsByClassName("html5-video-container")
+  )[0];
+  if (
+    videoContainer !== undefined &&
+    videoContainer.firstChild instanceof HTMLVideoElement
+  ) {
+    let videoPlayer = videoContainer.firstChild;
+    console.log("DEBUG: found video player");
+    videoPlayer.addEventListener("pause", () => {
+      console.log("paused");
+    });
+    videoPlayer.addEventListener("play", () => {
+      console.log("playing");
+    });
+    videoPlayer.addEventListener("timeupdate", () => {
+      // if (videoPlayer.currentTime === videoPlayer.duration) {
+        // get ready for skip ad button
+        const skipAdButton = Array.from(
+          document.getElementsByClassName("ytp-ad-skip-button ytp-button")
+        )[0];
+        if (skipAdButton !== undefined) {
+          skipAdButton.click();
+        }
+      // }
+      console.log("update: " + videoPlayer.currentTime);
+    });
+
+    const videoWrapper = videoContainer.parentNode;
+    if (String(videoWrapper.className).includes("ad-showing")) {
+      console.log(videoPlayer.duration);
+      videoPlayer.currentTime = videoPlayer.duration - 3;
+      videoPlayer.pause();
+      videoPlayer.play();
+    }
+  } else {
+    console.log("DEBUG: video player not found");
+  }
+}
 
 function seekAd() {
   console.log("DEBUG: reaching");
